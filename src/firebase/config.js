09de18392d1a6b63e9679ee96d690ecd62563132
innovationwebservice <innovation.web.services@gmail.com>
2021-10-17 22:14:45 -0700

    Google Login

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyChlhGKuVZmM5JQ0QVk9YQe_f0UtrBA440",
+  authDomain: "react-redux-firebase-eco-9423c.firebaseapp.com",
+  projectId: "react-redux-firebase-eco-9423c",
+  storageBucket: "react-redux-firebase-eco-9423c.appspot.com",
+  messagingSenderId: "417147729061",
+  appId: "1:417147729061:web:24c16e840542971c37894c",
+};
