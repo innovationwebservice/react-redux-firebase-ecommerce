7ec8b360e5d41fd466cb5971a6e06f2f4fb93f6d
innovationwebservice <innovation.web.services@gmail.com>
2021-10-26 20:46:46 -0700

    email and password auth using firebase

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./styles.scss";
+import { Link } from "react-router-dom";
+import footerLogo from "./../../assets/footer-iws-header-logo.png";
 
 const Footer = (props) => {
   return (
     <footer className="footer">
-      <div className="wrap">My Awesome Footer content!!</div>
+      <div className="wrap">
+        <Link to="https://innovationwebservice.com" target="_blank">
+          <img
+            src={footerLogo}
+            title="Innovation Web Service Logo"
+            alt="Innovation Web Service Logo"
+          />
+        </Link>
+      </div>
     </footer>
   );
 };
