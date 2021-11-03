aea0d7d968204b649fe51ff19d6fd6a69077fad6
innovationwebservice <innovation.web.services@gmail.com>
2021-11-02 19:31:15 -0700

    Redux Hooks

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,32 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
+import {
+  signInUser,
+  signInWithGoogle,
+  resetAllAuthForms,
+} from "./../../redux/User/user.actions";
 
 import "./styles.scss";
-import { signInWithGoogle, auth } from "./../../firebase/utils";
 
 import AuthWrapper from "./../AuthWrapper";
 import FormInput from "./../forms/FormInput";
-import Buttons from "./../forms/Button";
+import Button from "./../forms/Button";
+
+const mapState = ({ user }) => ({
+  signInSuccess: user.signInSuccess,
+});
 
 const SignIn = (props) => {
+  const { signInSuccess } = useSelector(mapState);
+  const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    if (signInSuccess) {
+      resetForm();
+      dispatch(resetAllAuthForms());
+      props.history.push("/");
+    }
+  }, [signInSuccess]);
 
   const resetForm = () => {
     setEmail("");
     setPassword("");
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(signInUser({ email, password }));
+  };
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      resetForm();
-      props.history.push("/");
-    } catch (err) {
-      //console.log(err)
-    }
+  const handleGoogleSignIn = () => {
+    dispatch(signInWithGoogle());
   };
 
   const configAuthWrapper = {
@@ -53,11 +69,11 @@ const SignIn = (props) => {
             handleChange={(e) => setPassword(e.target.value)}
           />
 
-          <Buttons type="submit">LogIn</Buttons>
+          <Button type="submit">LogIn</Button>
 
           <div className="socialSignin">
             <div className="row">
-              <Buttons onClick={signInWithGoogle}>Sign in with Google</Buttons>
+              <Button onClick={handleGoogleSignIn}>Sign in with Google</Button>
             </div>
           </div>
 
