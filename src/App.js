f45e445f5609c1c8a04f0730027da8be03f922b4
innovationwebservice <innovation.web.services@gmail.com>
2021-11-10 00:22:15 -0800

    Admin Manage Products

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import WithAdminAuth from "./hoc/withAdminAuth";
 //Layouts
 import MainLayout from "./layouts/MainLayout";
 import HomepageLayout from "./layouts/HomepageLayout";
+import AdminLayout from "./layouts/AdminLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 //pages
 import Homepage from "./pages/Homepage";
@@ -71,9 +73,9 @@ const App = (props) => {
           path="/dashboard"
           render={() => (
             <WithAuth>
-              <MainLayout>
+              <DashboardLayout>
                 <Dashboard />
-              </MainLayout>
+              </DashboardLayout>
             </WithAuth>
           )}
         />
@@ -81,9 +83,9 @@ const App = (props) => {
           path="/admin"
           render={() => (
             <WithAdminAuth>
-              <MainLayout>
+              <AdminLayout>
                 <Admin />
-              </MainLayout>
+              </AdminLayout>
             </WithAdminAuth>
           )}
         />
